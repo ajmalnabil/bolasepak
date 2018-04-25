6d920bfa440d7993ee779ee5382fba344c7d998d
Ajmal Nabil <ajmalnabiljamal@gmail.com>
2018-04-25 13:56:57 +0800

    Initial Twitter gem

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "8rx9jym8bznpI1dPhhDEORJSP"
+  config.consumer_secret     = "6JmUBNvXCNDetjBfK51WNw39mqWRr3Yrvv2sv9pN81PxNcovEw"
+  config.access_token        = "37025099-kXm1cp9HfzMc4ubPys1g6jRYxoMHcHUePWlUId57r"
+  config.access_token_secret = "jEaZdvZtV5yoOtaCPqBwuhJDT41gUEmAOGk2ZXNrlzroL"
+end
